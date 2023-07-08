6ae8683c7feb153961309fc7ba12d1fa768f0eb3
hightop0924 <hightop.dev.hk@gmail.com>
2023-07-09 04:55:38 +0900

    utils changed

--- src/utils.rs
+++ src/utils.rs
@@ -7,9 +7,13 @@ pub fn ct_slice_compare(s1: &[u8], s2: &[u8]) -> bool {
     bool::from(s1.ct_eq(s2))
 }
 
+pub fn ct_slice_compare1(s1: &[u16], s2: &[u16]) -> bool {
+    bool::from(s1.ct_eq(s2))
+}
+
 pub fn create_hashed_password(s1: &str) -> [u8; VIEWING_KEY_SIZE] {
     Sha256::digest(s1.as_bytes())
         .as_slice()
         .try_into()
-        .expect("Wrong password length")
+        .expect("Password length is invalid")
 }
